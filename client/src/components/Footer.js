7ac70970e5b7c579ffc181e102b02ef36ac113e9
Mathew B <bogaert@email.arizona.edu>
2021-03-22 15:49:39 -0700

    added material ui grid

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  top: {
+    paddingTop: "15px",
+  },
+}));
 
 const Copyright = () => {
+
+  const classes = useStyles();
+
   return (
-    <Typography variant="body2" color="textSecondary" align="center">
+    <Typography variant="body2" color="textSecondary" align="center" className={classes.top}>
       {'Copyright © '}
       <Link color="inherit" href="/">
         Final Project
